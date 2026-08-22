b32e8760787579190db1e49dddfa03b8fce7064d
wagenheimer <wagenheimer@gmail.com>
2026-08-21 23:03:18 -0300

    fix: rewrite UIBlockerOverlay with proper namespace/class structure

--- Runtime/UIBlockerOverlay.cs
+++ Runtime/UIBlockerOverlay.cs
@@ -3,44 +3,50 @@ using UnityEngine.UI;
 
 namespace Wagenheimer.RewiredHelper
 {
-    private const int TopmostSortingOrder = 32767;
-
-    private static GameObject _overlay;
-    private static bool _enabled;
-
-    public static bool Enabled
+    /// <summary>
+    /// Fullscreen transparent Image (raycastTarget=true) on a topmost overlay canvas.
+    /// When Enabled, it swallows all pointer input so nothing underneath is clickable.
+    /// </summary>
+    public static class UIBlockerOverlay
     {
-        get { return _enabled; }
-        set
+        private const int TopmostSortingOrder = 32767;
+
+        private static GameObject _overlay;
+        private static bool _enabled;
+
+        public static bool Enabled
         {
-            _enabled = value;
-            if (value) EnsureOverlay().SetActive(true);
-            else if (_overlay != null) _overlay.SetActive(false);
+            get { return _enabled; }
+            set
+            {
+                _enabled = value;
+                if (value) EnsureOverlay().SetActive(true);
+                else if (_overlay != null) _overlay.SetActive(false);
+            }
+        }
+
+        private static GameObject EnsureOverlay()
+        {
+            if (_overlay != null) return _overlay;
+
+            var canvasGo = new GameObject("UIBlockerOverlay", typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster));
+            var canvas = canvasGo.GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.sortingOrder = TopmostSortingOrder;
+            Object.DontDestroyOnLoad(canvasGo);
+
+            var imageGo = new GameObject("Blocker", typeof(Image));
+            imageGo.transform.SetParent(canvasGo.transform, false);
+            var image = imageGo.GetComponent<Image>();
+            image.color = new Color(0f, 0f, 0f, 0f); // fully transparent
+            image.raycastTarget = true; // swallows all clicks
+            var rect = imageGo.GetComponent<RectTransform>();
+            rect.anchorMin = Vector2.zero;
+            rect.anchorMax = Vector2.one;
+            rect.sizeDelta = Vector2.zero;
+
+            _overlay = canvasGo;
+            return _overlay;
         }
     }
-
-    private static GameObject EnsureOverlay()
-    {
-        if (_overlay != null) return _overlay;
-
-        var canvasGo = new GameObject("UIBlockerOverlay", typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster));
-        var canvas = canvasGo.GetComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        canvas.sortingOrder = TopmostSortingOrder;
-        Object.DontDestroyOnLoad(canvasGo);
-
-        var imageGo = new GameObject("Blocker", typeof(Image));
-        imageGo.transform.SetParent(canvasGo.transform, false);
-        var image = imageGo.GetComponent<Image>();
-        image.color = new Color(0f, 0f, 0f, 0f); // fully transparent
-        image.raycastTarget = true; // swallows all clicks
-        var rect = imageGo.GetComponent<RectTransform>();
-        rect.anchorMin = Vector2.zero;
-        rect.anchorMax = Vector2.one;
-        rect.sizeDelta = Vector2.zero;
-
-        _overlay = canvasGo;
-        return _overlay;
-    }
-}
 }
